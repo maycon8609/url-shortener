0087cdc4e4b8ab54f62bfa6c9a66170aa9410407
maycon silva <maycon8609@gmail.com>
2024-11-22 15:39:50 -0300

    feat: added handleRequest lambda function

--- src/main/java/com/maycon/Main.java
+++
@@ -1,7 +0,0 @@
-package com.maycon;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/maycon/createUrlShortener/Main.java
@@ -0,0 +1,37 @@
+package com.maycon.createUrlShortener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = (String) input.get("body");
+
+        Map<String, String> bodyMap;
+        try {
+            bodyMap = objectMapper.readValue(body, Map.class);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error parsing JSON body: ", exception);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+        response.put("originalUrl", originalUrl);
+        response.put("expirationTime", expirationTime);
+
+        return response;
+    }
+}
